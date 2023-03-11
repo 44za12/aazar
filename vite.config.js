acb8120d99f462358b84498f6aad5bfbd0c03d0b
Aazar <44za12@gmail.com>
2023-03-11 23:23:34 +0530

    Make PWA

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import { sveltekit } from '@sveltejs/kit/vite'
 export default defineConfig({
   plugins: [sveltekit()],
   // allows vite access to ./posts
+  define: {
+		'process.env.NODE_ENV': '"production"',
+	},
   server: {
     fs: {
       allow: ['./']
